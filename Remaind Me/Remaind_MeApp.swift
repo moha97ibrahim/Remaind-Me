6190c4e3d161fedba737e2fa02211c49fbe6c6a8
mohammed.ibrahim <mohammed.ibrahim@kydonlsg.com>
2021-05-08 22:34:07 +0530

    Initial Commit

---
+++ Remaind Me/Remaind_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remaind_MeApp.swift
+//  Remaind Me
+//
+//  Created by Mohammed Ibrahim on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Remaind_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
